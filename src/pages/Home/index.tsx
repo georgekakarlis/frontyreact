787c42518ff826b5af120594638e18d121702078
George Kakarlis <georgekakarlis@yahoo.com>
2022-10-04 11:03:02 +0200

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/ui/Navbar";
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-indigo-500">
+      <Navbar />
+      <p>this is home page</p>
+    </div>
+    </>
+  )
+}
